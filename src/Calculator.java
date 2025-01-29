f9f2c9e0ca0604a0a246ca33a17074a201527293
KarimovDaniyar <karimovdaniar224@gmail.com>
2025-01-29 12:27:42 +0600

    first commit

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,11 +1,55 @@
-public class  Calculator {
+import java.util.Scanner;
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean cont = true;
+        while(cont){
+            System.out.println("first number: ");
+            int n1 = scanner.nextInt();
+            System.out.println("second number: ");
+            int n2 = scanner.nextInt();
+            System.out.println("choose (+, -, *, /): ");
+            String action = scanner.next();
+            double result = 0;
+            boolean valid = true;
 
-    public void add(int a, int b){
-        System.out.println(a+b);
-    }
+            switch (action) {
+                case "+":
+                    result = n1 + n2;
+                    break;
+                case "-":
+                    result = n1 - n2;
+                    break;
+                case "*":
+                    result = n1 * n2;
+                    break;
+                case "/":
+                    if (n2 != 0) {
+                        result = n1 / n2;
+                    } else {
+                        System.out.println("Error: division by zero");
+                        valid = false;
+                    }
+                    break;
+                default:
+                    System.out.println("please write correct action");
+                    valid = false;
+            }
+            if (valid){
+                System.out.println(result);
+            }
+            System.out.println("continue ? ");
+            String answer = scanner.nextLine();
 
-    public void add(int a, double b){
-        System.out.println(a+b);
+            if (answer.equalsIgnoreCase("no")){
+                cont = false;
+                System.out.println("Good bye");
+            }if (answer.equalsIgnoreCase("yes")){
+                cont = true;
+            }
+        }
+        scanner.close();
     }
 }
+
 
