a153cb1da46db441ef9c021990d3eb99fcbb5af4
KarimovDaniyar <karimovdaniar224@gmail.com>
2025-01-29 12:10:14 +0600

    класс для добавление книг

---
+++ src/Calculator.java
@@ -0,0 +1,11 @@
+public class  Calculator {
+
+    public void add(int a, int b){
+        System.out.println(a+b);
+    }
+
+    public void add(int a, double b){
+        System.out.println(a+b);
+    }
+}
+
